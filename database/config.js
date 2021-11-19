addca739f4aa0da663abe66362d0fde1398ae95b
Duy Tran <tranduy14884@gmail.com>
2021-11-19 22:46:18 +0700

    asdasdsa

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, () =>
+    await mongoose.connect(process.env.MONGO_URI_CLOUD, () =>
       console.log("DB connected !!!")
     );
   } catch (error) {
